501d378bbfb81bdd1d17ecead202a1c08e0f020a
daei98 <78364964+daei98@users.noreply.github.com>
2021-07-22 21:11:47 +0430

    initial commit

---
+++ resources/JS/script.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+var firstName = 'ali';
+var age = '15';
+//comment
+/*
+also commment*/
+console.log('My name is '+ firstName +' and I\'m '+ age +' years old.');
+alert('This is an alert');
+var input = prompt('Enter your input');
