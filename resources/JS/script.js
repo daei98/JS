5f44813b3a32a008bc3f20191b4af5be240088fb
daei98 <78364964+daei98@users.noreply.github.com>
2021-07-24 01:37:54 +0430

    Operators + If Statements

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -11,9 +11,33 @@ document.getElementById("makeItRed").style.color = "red"
 document.write('this is also a test')
 var x= 10;
 x += 5;
-x -= 2;
+x -= 5;
 x *= 5;
 x /= 5;
 x %= 5;
 //var n = x.toString; // Convert a number to a string
 //var n = x.toFixed(2) // Convert a number into a string, rounding the number to keep only two decimals
+var bool = x==2;//equal to
+var bool = x===2;//equal value and equal type
+var bool = x==2;//not equal
+var bool = x==2;//not equal value or not equal type
+var bool = x>=2;//greater than or equal to
+console.log(x);
+/*if(bool==true){
+    alert('TRUE');
+}
+if(x!=0){
+    alert('x is not 0');
+}
+else{
+    console.log('Else');
+}*/
+if(bool==true || x!=0){
+    alert('TRUE');
+}
+else if(x!=0 && bool==true){
+    alert('x is not 0');
+}
+else{
+    console.log('Else');
+}
